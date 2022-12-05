5eb01892c5af18ea1ddb97017e1de6458c06ae1b
Patrick Kalkman <patrick@simpletechture.nl>
2022-12-05 22:50:18 +0100

    finished day4 part2

--- day4/day4-2.py
+++ day4/day4-2.py
@@ -10,29 +10,13 @@ for section_assignment_pair in section_assignments:
     section1_number1, section1_number2 = section1.split('-')
     section1_number1 = int(section1_number1)
     section1_number2 = int(section1_number2)
-    # pairs.append((section1, int(section1_number1), int(section1_number2)))
     section2_number1, section2_number2 = section2.split('-')
     section2_number1 = int(section2_number1)
     section2_number2 = int(section2_number2)
 
-    if section1_number1 >= section2_number1 and section1_number2 <= section2_number2:
-        contain_counter += 1
-    elif section2_number1 >= section1_number1 and section2_number2 <= section1_number2:
-        contain_counter += 1
+    for number in range(section1_number1, section1_number2 + 1):
+        if (number >= section2_number1 and number <= section2_number2):
+            contain_counter += 1
+            break
 
-print(contain_counter)
-    
-    # pairs.append((section2, int(section2_number1), int(section2_number2)))
-
-# contain_counter = 0
-# for pair_index1 in range(0, len(pairs)):
-#     for pair_index2 in range(0, len(pairs)):
-#         if pair_index1 != pair_index2:
-#             if pairs[pair_index2][1] >= pairs[pair_index1][1] and pairs[pair_index2][2] <= pairs[pair_index1][2]:
-#                 print(pairs[pair_index1][0] + " contains " + pairs[pair_index2][0])
-#                 contain_counter += 1
-
-
-
-print(len(pairs))
 print(contain_counter)
