3957ffac8c636208ca846143e5999b0fda67b3a1
Patrick Kalkman <patrick@simpletechture.nl>
2022-12-09 14:52:26 +0100

    completed part2 of day9

--- day9/day9-2.py
+++ day9/day9-2.py
@@ -1,6 +1,6 @@
 
 def read_input():
-    head_moves_file = open('./test-input2.txt', 'r')
+    head_moves_file = open('./input.txt', 'r')
     head_moves = head_moves_file.readlines()
     head_moves_file.close()
     return head_moves
@@ -105,7 +105,7 @@ def make_moves(start_position, matrix, head_moves, record_matrix, number_of_tail
     yx_move = (0, 0)
     move_index = 1
     for move in head_moves:
-        print(move)
+        print(move.strip())
         move_index += 1
         direction = move.split(' ')[0]
         places = int(move.split(' ')[1])
@@ -119,7 +119,7 @@ def make_moves(start_position, matrix, head_moves, record_matrix, number_of_tail
             yx_move = (1, 0)
 
         for i in range(0, places):
-            reset_matrix(matrix)
+            #reset_matrix(matrix)
             current_head_position = (current_head_position[0] + yx_move[0], current_head_position[1] + yx_move[1])
 
             head = current_head_position
@@ -133,23 +133,22 @@ def make_moves(start_position, matrix, head_moves, record_matrix, number_of_tail
 
             record_matrix[tails_position[8][0]][tails_position[8][1]] = '#'
             # print_matrix_focus(matrix,current_head_position)
-            print_matrix(matrix)
-            print("")
+            #print_matrix(matrix)
+            #print("")
 
     return current_head_position
 
 
-#matrix_size = (500, 500)
+matrix_size = (500, 500)
 #matrix_size = (6, 5)
-matrix_size = (27, 22)
+#matrix_size = (27, 22)
 head_moves = read_input()
 matrix = create_matrix(matrix_size[0], matrix_size[1])
 record_matrix = create_matrix(matrix_size[0], matrix_size[1])
-#start_position = (106, 89)
-start_position = (16, 11)
+start_position = (106, 89)
+#start_position = (16, 11)
 
 current_head_position = make_moves(start_position, matrix, head_moves, record_matrix, 9)
-print(current_head_position)
-print("")
-print_matrix(record_matrix)
+#print(current_head_position)
+#print_matrix(record_matrix)
 print(count_visited(record_matrix))
